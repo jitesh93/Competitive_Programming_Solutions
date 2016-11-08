9c5b57cb6ae1f4af8a013ade55884ccc0e70ba71
Amit Auddy <amitauddy94@gmail.com>
2016-11-08 18:01:55 +0530

    fixed indentation

--- Java/hackerearth/monk_and_circular_distance.java
+++ Java/hackerearth/monk_and_circular_distance.java
@@ -4,63 +4,65 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 import java.util.Arrays;
 
-class TestClass {
-	static class FastReader {
-    BufferedReader br;
-    StringTokenizer st;
+class TestClass 
+{
+    static class FastReader 
+    {
+        BufferedReader br;
+        StringTokenizer st;
 
-    public FastReader() {
-      br = new BufferedReader(new
-      InputStreamReader(System.in));
-    }
-
-    String next() {
-      while (st == null || !st.hasMoreElements()) {
-        try {
-          st = new StringTokenizer(br.readLine());
+        public FastReader() {
+            br = new BufferedReader(new
+            InputStreamReader(System.in));
         }
-        catch (IOException  e) {
-          e.printStackTrace();
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch(IOException  e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
         }
-      }
-      return st.nextToken();
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
     }
 
-    int nextInt() {
-      return Integer.parseInt(next());
-    }
-
-    long nextLong() {
-      return Long.parseLong(next());
-    }
-  }
-
-  public static void main(String args[] ) throws IOException {
-    FastReader in = new FastReader();
-    int N = in.nextInt();
-    long[] A = new long[N];
-    for(int i = 0; i < N; i++) {
-      long x = in.nextLong();
-      long y = in.nextLong();
-      A[i] = (x * x) + (y * y);
-    }
-    Arrays.sort(A);
-    int Q = in.nextInt();
-    while(Q-->0) {
-      int count = 0;
-      long R = in.nextLong();
-      int low = 0, high = N, mid = 0;
-      while(low < high) {
-      	mid = (low + high)/2;
-        if(Math.sqrt(A[mid]) > R) {
-         high = mid;
+    public static void main(String args[] ) throws IOException {
+        FastReader in = new FastReader();
+        int N = in.nextInt();
+        long[] A = new long[N];
+        for(int i = 0; i < N; i++) {
+            long x = in.nextLong();
+            long y = in.nextLong();
+            A[i] = (x * x) + (y * y);
         }
-        if(Math.sqrt(A[mid]) <= R) {
-         low = mid + 1;
+        Arrays.sort(A);
+        int Q = in.nextInt();
+        while(Q-->0) {
+            int count = 0;
+            long R = in.nextLong();
+            int low = 0, high = N, mid = 0;
+            while(low < high) {
+                mid = (low + high)/2;
+                if(Math.sqrt(A[mid]) > R) {
+                    high = mid;
+                }
+                if(Math.sqrt(A[mid]) <= R) {
+                    low = mid + 1;
+                }
+                count = low;
+            }
+            System.out.println(count);
         }
-        count = low;
-      }
-      System.out.println(count);
     }
-  }
 }
